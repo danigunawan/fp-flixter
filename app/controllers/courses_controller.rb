d4ca4b3ad6260138dec99e3f8683b92c10e84a5d
Oleg Bulkin <o.bulkin@gmail.com>
2016-03-11 01:41:20 +0000

    Created an individual course view for students and fixed a pagination bug on the overall course view

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -1,5 +1,10 @@
 class CoursesController < ApplicationController
   def index
     @courses = Course.page(params[:page]).per(3)
+    return redirect_to courses_path(@courses.num_pages) if params[:page] && params[:page].to_i > @courses.num_pages 
+  end
+
+  def show
+    @course = Course.find(params[:id])
   end
 end
