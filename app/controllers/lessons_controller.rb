b814ef27bfc4e5c752b5f19bf96dfa0917e7ec37
Oleg Bulkin <o.bulkin@gmail.com>
2016-03-22 01:28:29 +0000

    Limited access to lesson details to enrolled users and course owners

--- app/controllers/lessons_controller.rb
+++ app/controllers/lessons_controller.rb
@@ -1,5 +1,17 @@
 class LessonsController < ApplicationController
+  before_action :authenticate_user!
+  before_action :require_enrolled_user_or_owner
+
   def show
-    @lesson = Lesson.find params[:id]
+  end
+
+  private
+  def current_lesson
+    @lesson ||= Lesson.find params[:id]
+  end
+
+  def require_enrolled_user_or_owner
+    current_course = current_lesson.section.course
+    redirect_to course_path(current_course), alert: "You must be enrolled in this course to access the lessons." unless current_user.enrolled_in?(current_course) || current_user.courses.include?(current_course)
   end
 end
