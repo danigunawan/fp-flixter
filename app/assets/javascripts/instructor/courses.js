d2d3397f90751a6d0f8b05a0fd2dc54bf825ae4c
Oleg Bulkin <o.bulkin@gmail.com>
2016-03-23 20:58:02 +0000

    Added some styling associated with sorting lessons to the instructor course view

--- app/assets/javascripts/instructor/courses.js
+++ app/assets/javascripts/instructor/courses.js
@@ -1,15 +1,15 @@
 $(document).on("page:change", function() {
 	var sortableLessons = $(".sortable-lessons");
 	if (sortableLessons.length > 0) {
+		// Make the lesson elements in a section sortable via dragging if the page is loaded and correct
 		sortableLessons.sortable({
 			update: function(event, ui) {
+				// Call the instructor/lessons controller with the lesson's new sort order every time the user has finished dragging a lesson
 				$.ajax({
 					method: "PATCH",
 					url: ui.item.data("update-url"),
 					data: {sort_order_position: ui.item.index()}
 				});
-				console.log(ui.item.index());
-				console.log(ui.item.data("update-url"));
 			}
 		});
 	}
