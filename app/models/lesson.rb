8d5f6dda96e60a43c9f89798c159e5f02cdbb373
Oleg Bulkin <o.bulkin@gmail.com>
2016-03-25 20:57:49 +0000

    Added a next lesson link to the individual lesson view

--- app/models/lesson.rb
+++ app/models/lesson.rb
@@ -9,4 +9,13 @@ class Lesson < ActiveRecord::Base
   validates :title, presence: true
   validates :subtitle, presence: true
   validates :video, presence: true
+
+  def next_lesson
+    next_lesson = section.lessons.where("sort_order > ?", sort_order).rank(:sort_order).first
+    unless next_lesson 
+      next_section = section.next_section_with_lessons
+      next_lesson = next_section.lessons.rank(:sort_order).first if next_section
+    end
+    next_lesson
+  end
 end
--- app/models/section.rb
+++ app/models/section.rb
@@ -6,4 +6,13 @@ class Section < ActiveRecord::Base
   ranks :sort_order, with_same: :course_id 
 
   validates :title, presence: true
+
+  def next_section_with_lessons
+    next_section = self
+    while (next_section)
+      next_section = course.sections.where("sort_order > ?", next_section.sort_order).rank(:sort_order).first
+      return next_section if next_section && next_section.lessons.present?
+    end
+    next_section
+  end
 end
